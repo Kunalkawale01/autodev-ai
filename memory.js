1dfee5944aa34b70d8c2ddd9d67b91533b1c3a69
Kunal kawale <kunalkawale62@gmail.com>
2026-03-29 11:42:34 +0530

    Update memory.js

--- memory.js
+++ memory.js
@@ -1,13 +1,7 @@
 import fs from "fs";
 
 export function saveMemory(entry) {
-  let data = { history: [] };
-
-  try {
-    data = JSON.parse(fs.readFileSync("memory.json"));
-  } catch {}
-
+  const data = JSON.parse(fs.readFileSync("memory.json"));
   data.history.push(entry);
-
   fs.writeFileSync("memory.json", JSON.stringify(data, null, 2));
 }
